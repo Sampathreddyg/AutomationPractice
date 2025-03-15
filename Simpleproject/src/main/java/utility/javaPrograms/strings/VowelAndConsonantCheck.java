89aea5aa5ee70c5c8467f08768afe73a567387a2
sampathreddyg <sampathreddy.guttireddy@gmail.com>
2025-03-15 17:59:17 +0530

    Vowel and Consonant

---
+++ Simpleproject/src/main/java/utility/javaPrograms/strings/VowelAndConsonantCheck.java
@@ -0,0 +1,30 @@
+package utility.javaPrograms.strings;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class VowelAndConsonantCheck {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter any String ");
+		String input = sc.nextLine().toLowerCase();
+		sc.close();
+		vowelConsonantCount(input);
+	}
+
+	static void vowelConsonantCount(String str) {
+		str = str.toLowerCase();
+		int vowels = 0, consonants = 0;
+		for(char c : str.toCharArray()) {
+			if(c>='a' && c<='z') {
+				if(c=='a' || c=='e' || c=='i' || c=='o' || c=='u') {
+					vowels++;
+				}else {
+					consonants++;
+				}
+			}
+		}
+		System.out.println("In given string No of Vowels: "+vowels+ " and No of Consonants: "+consonants);
+	}
+}
