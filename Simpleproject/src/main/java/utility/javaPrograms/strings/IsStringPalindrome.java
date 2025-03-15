bb2ee0aa886b14b84a0866ff5bea4cd89c062b9d
sampathreddyg <sampathreddy.guttireddy@gmail.com>
2025-03-15 15:34:40 +0530

    String Palindrome

---
+++ Simpleproject/src/main/java/utility/javaPrograms/strings/IsStringPalindrome.java
@@ -0,0 +1,27 @@
+package utility.javaPrograms.strings;
+
+import java.util.Scanner;
+
+public class IsStringPalindrome {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter any String ");
+		String input = sc.nextLine();
+		sc.close();
+		System.out.println("Given string is Palindrome : "+isStringPalindrome(input));
+	}
+
+	static boolean isStringPalindrome(String inputString) {
+		int start = 0, end = inputString.length()-1;
+		
+		while(start<end) {
+			if(inputString.charAt(start)!=inputString.charAt(end)){
+				return false;
+			}
+			start++;
+			end--;
+		}
+		return true;
+	}
+}
